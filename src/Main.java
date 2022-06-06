0f256c975be60907be6a0685ae5756db5e68aff7
hanawinchester <hana.rose.winchester@gmail.com>
2022-06-05 21:14:47 -0400

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int integer = 7;
+        int modResult = integer % 2;
+        if (modResult == 0) {
+            System.out.println("Even");
+
+    }
+        else {
+            System.out.println("Odd");
+        }
+    }
+}
